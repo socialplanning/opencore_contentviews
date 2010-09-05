ece70eb7028abff9554fab2faf6847b2951cd7ce
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-09-05 23:07:54 +0000

    * add convenience view at /openplans/@@manage-site-content-blocks for site admins (cmf.ModifyPortalContent)    to set the wiki pages pointed to for each content block that exists, without dropping into the ZMI to use    the properties tab (and, providing a layer of decoupling in case the persistence backend changes later)
       though the raw properties are stored as full traversal paths from the opencore site root, this management
       form asks the user for a project ID and page IDs within that project.  in other words, it assumes (but only
       on a UI level) that all content blocks will be pulled in from the same project's wiki. this seems like a
       good assumption -- a single project can centralize the site content development team.
    
       if that assumption is broken by a site, it can always use the lower-level ZMI forms.
    
     * when rendering one of these SiteContentBlock views, if the requesting user is a site admin, add a PSM
       telling the user where the block's content originated, and linking to the aforementioned management form.
    
       TODO: i18n. always i18n todo.
    
    
    
    
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/opencore_sitecontent/trunk@27516 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore_sitecontent/views.py
+++ opencore_sitecontent/views.py
@@ -1,5 +1,34 @@
 from opencore.browser.base import BaseView
 from opencore.interfaces import IOpenPage
+from opencore.browser.formhandler import post_only
+
+class ManageSiteContentBlocks(BaseView):
+    def handle_request(self):
+        if self.request.environ['REQUEST_METHOD'] != "POST":
+            return
+
+        project = self.request.form['project']
+        project = 'projects/%s' % project
+        
+        for key in self.keys():
+            value = self.request.form[key]
+            if not value: 
+                continue
+            value = '%s/%s' % (project, value)
+            self.set_source_page(key, value)
+
+    def set_source_page(self, key, path):
+        source_page = self.portal.getProperty(key)
+        if source_page is None:
+            self.portal.manage_addProperty(key, path, "string")
+        else:
+            self.portal.manage_changeProperties(**{key: path})
+    
+    def keys(self):
+        return [
+            "sitecontent_aboutblock",
+            "sitecontent_becomingamember",
+            ]
 
 class SiteContentBlock(BaseView):
 
@@ -9,32 +38,38 @@ class SiteContentBlock(BaseView):
             raise KeyError("Property %s has not been set on the OpenCore site root" % self.key)
         page = self.portal.restrictedTraverse(path)
         return IOpenPage(page)
-        
-    def set_source_page(self, path):
-        if self.source_page() is None:
-            self.portal.manage_addProperty(self.key, path, "string")
-        else:
-            self.portal.manage_changeProperties(**{self.key: path})
 
     def __call__(self):
+        is_admin = self.admin_loggedin()
         try:
             try:
                 page = self.source_page()
             except KeyError, e:
-                if not self.admin_loggedin():
+                if not is_admin:
                     raise
                 return str(e)
             except AttributeError, e:
-                if not self.admin_loggedin():
+                if not is_admin:
                     raise
                 return "No wiki page exists at %s" % self.portal.getProperty(self.key)
             except TypeError, e:
-                if not self.admin_loggedin():
+                if not is_admin:
                     raise
                 return "The page at %s is not a wiki page, but we need a wiki page" % (
                     self.portal.getProperty(self.key))
         except:
             return ''
+        if is_admin:
+            title = page.Title()
+            url = page.absolute_url()
+            manage_url = '%s/%s' % (self.portal.absolute_url(),
+                                    'manage-site-content-blocks')
+            flash = """
+The content of this page block is being pulled in from <a href="%s">the wiki page %s</a>. 
+As a site administrator, you can set a different wiki page to pull content 
+from using <a href="%s">this form</a>.""" % (
+                url, title, manage_url)
+            self.addPortalStatusMessage(flash)
         return page.restrictedTraverse("@@raw-view")()
             
 class AboutBlock(SiteContentBlock):
