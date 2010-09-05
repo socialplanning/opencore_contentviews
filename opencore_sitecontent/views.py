fdde928807d2e5ede8fcc1c16764d01d31ae9c7d
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2010-09-05 18:45:24 +0000

    baroque error handling where site admins hear errors but regular people see a blank page
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/opencore_sitecontent/trunk@27515 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore_sitecontent/views.py
+++ opencore_sitecontent/views.py
@@ -1,12 +1,15 @@
 from opencore.browser.base import BaseView
+from opencore.interfaces import IOpenPage
 
 class SiteContentBlock(BaseView):
 
     def source_page(self):
         path = self.portal.getProperty(self.key)
+        if not path:
+            raise KeyError("Property %s has not been set on the OpenCore site root" % self.key)
         page = self.portal.restrictedTraverse(path)
-        return page
-
+        return IOpenPage(page)
+        
     def set_source_page(self, path):
         if self.source_page() is None:
             self.portal.manage_addProperty(self.key, path, "string")
@@ -14,7 +17,24 @@ class SiteContentBlock(BaseView):
             self.portal.manage_changeProperties(**{self.key: path})
 
     def __call__(self):
-        page = self.source_page()
+        try:
+            try:
+                page = self.source_page()
+            except KeyError, e:
+                if not self.admin_loggedin():
+                    raise
+                return str(e)
+            except AttributeError, e:
+                if not self.admin_loggedin():
+                    raise
+                return "No wiki page exists at %s" % self.portal.getProperty(self.key)
+            except TypeError, e:
+                if not self.admin_loggedin():
+                    raise
+                return "The page at %s is not a wiki page, but we need a wiki page" % (
+                    self.portal.getProperty(self.key))
+        except:
+            return ''
         return page.restrictedTraverse("@@raw-view")()
             
 class AboutBlock(SiteContentBlock):
